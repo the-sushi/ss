cae9ad791a0086f9558d2e435b98a6932f8ad802
the-sushi <joshpritsker@protonmail.com>
2018-11-14 20:33:57 -0500

    uhh shit that file wasn't needed

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 if [ "$1" == "debug" ]
 then
-	$CC --std=c90 *.c -ledit -o ss -g
+	$CC --std=c90 -Wall *.c -ledit -o ss -g
 	cppcheck . --std=c89
 else
-	$CC --std=c90 -O3 *.c -ledit -o ss
+	$CC --std=c90 -Wall -O3 *.c -ledit -o ss
 fi
